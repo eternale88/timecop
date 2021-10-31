2571d2d5cecb9084499972be5d0281e6cb88a96e
eternale88 <ethanrobbins@ymail.com>
2021-10-31 16:15:56 -0400

    task component-refactor-hook-start-css

--- src/custom-hooks/index.js
+++ src/custom-hooks/index.js
@@ -19,21 +19,18 @@ export const useTasks = (selectedProject) => {
 		.where('userId', '==', 'e3dger393ngri')
 
 		//if we have a project and it doesn't exist in the collated tasks
-		if(selectedProject && !collatedTasksExist(selectedProject)) {
-			(unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
-		} else if(selectedProject === 'TODAY') {
-			//retrieve today's projects
-			(unsubscribe = unsubscribe.where(
-				'date',
-				 '==',
-					moment().format('DD/MM/YYYY')
-			 ))
-		} else if(selectedProject === 'INBOX' || selectedProject === 0) {
-			//new projects?
-			(unsubscribe = unsubscribe.where('date', '==', ''))
-		} else {
-			unsubscribe
-		}
+		unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where(
+            'date',
+            '==',
+            moment().format('DD/MM/YYYY')
+          ))
+        : selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
 
 		unsubscribe = unsubscribe.onSnapshot(snapshot => {
 			//getting docs from the firestore
@@ -46,34 +43,16 @@ export const useTasks = (selectedProject) => {
 			setTasks(
 				//get next 7 day projects back, if the difference in the task and todays date is less than 7 we know it's one of the next 7 days task and return it, else mark it as not archived and give them back
 				selectedProject === 'NEXT_7'
-				? newTasks.filter((task) => {
-					moment(task.date, 'DD-MM-YYY').diff(moment(), 'days') <= 7 && task.archived !== true
-				})
-				: newTasks.filter((task) => {
-					task.archived !== true
-				})
-			)
-
-			//our archived tasks
-			setArchivedTasks(newTasks.filter((task) => {
-				task.archived !== false
-			}))
+          ? newTasks.filter(
+              task =>
+                moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 
+								&& moment(task.date).diff(moment(), 'days') >= 0
+                && task.archived !== true
+            )
+          : newTasks.filter(task => task.archived !== true)
+      );
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
 		})
-
-		//refactored above from this ternary
-		// selectedProject && !collatedTasksExist(selectedProject) ?
-		// //retrieve the project if it's there
-		// (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
-		// : selectedProject === 'TODAY'
-		// ? (unsubscribe = unsubscribe.where(
-		// 	'date',
-		// 	 '==',
-		// 	  moment().format('DD/MM/YYYY')
-		// 		))
-		// 		: selectedProject === 'INBOX' || selectedProject === 0
-		// 		? (unsubscribe = unsubscribe.where('date', '==', ''))
-		// 		: unsubscribe
-
 		//we unsubscribe because we don't want to be looking for projects all the time, we only want to look when there's a new selectedProject
 		//a useEffect cleanup function
 		return () => unsubscribe()
@@ -94,7 +73,7 @@ export const usePojects = () => {
 		.orderBy('projectId')
 		.get()
 		.then((snapshot) => {
-			const allProjects = snapshot.docs.map(project => ({
+			const allProjects = snapshot.docs.map((project) => ({
 				...project.data(),
 				//if want to delete must alway pass docId
 				docId: project.id,
