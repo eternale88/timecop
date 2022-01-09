96a1523d38d61f172c88f6acb2c1a38372e43f9b
eternale88 <ethanrobbins@ymail.com>
2022-01-09 16:37:39 -0500

    more testing

--- src/__tests__/AddTask.spec.js
+++ src/__tests__/AddTask.spec.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import { AddTask } from '../components/AddTask'
 import { firebase } from '../firebase';
 import { useSelectedProjectValue } from '../context';
+import { ProjectsProvider, SelectedProjectProvider, SelectedProjectContext, ProjectsContext } from '../context/selected-project-context'
 
+
+function renderAddTaskWithContextWrapper(value) {
+  return render(
+    <SelectedProjectProvider>
+					<SelectedProjectContext.Consumer>
+					{(value) => <AddTask {...value} />	}
+					</SelectedProjectContext.Consumer>
+		</SelectedProjectProvider>
+  )
+}
+ beforeEach(cleanup)
+useSelectedProjectValue.mockReturnValue({selectedProject: 1 })
 jest.mock('../context', () => ({
 	useSelectedProjectValue: jest.fn(() => ({selectedProject: 1 }))
 }))
@@ -18,4 +31,29 @@ jest.mock('../firebase', () => ({
   },
 }));
 
-beforeEach(cleanup)
+describe('<AddTask/>', () => {
+	afterEach(() => {
+		jest.clearAllMocks()
+	})
+
+	describe('Success', () => {
+		it('renders <AddTask/> component', () => {
+			const { queryByTestId } = renderAddTaskWithContextWrapper()
+			expect(queryByTestId('add-task-comp')).toBeTruthy()
+		})
+
+		it('renders the <AddTask /> quick overlay', () => {
+
+			const { queryByTestId } = render(
+				<SelectedProjectProvider>
+							<SelectedProjectContext.Consumer>
+							{(value) => <AddTask {...value} />	}
+		
+							</SelectedProjectContext.Consumer>
+						</SelectedProjectProvider>
+			)      
+			expect(queryByTestId('quick-add-task')).toBeFalsy()
+    })
+
+	 })
+})
